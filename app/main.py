c02eef31e4135f3c1f0a8966625129789f814919
Marco Mercado <is.marco.mercado@gmail.com>
2025-08-08 16:26:44 -0600

    Added cors support

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import download
 
 
@@ -9,6 +10,19 @@ app = FastAPI(
 )
 
 app.include_router(download.router)
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:4173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
